6c0c66d44e689c46eeff730513d1fb938e3222b5
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2020-05-03 01:01:04 +0300

    Return json as response.

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root():
+    return jsonify([
+        {
+            "id": "1",
+            "title": "First post",
+            "text": "Some text",
+        },
+        {
+            "id": "2",
+            "title": "Second post",
+            "text": "Some text",
+        }])
 
 
 if __name__ == '__main__':
