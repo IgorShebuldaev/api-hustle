07cb7197290514ee9bd2556d3db204d152ba8185
psavchuk <savchukdev@gmail.com>
2020-05-03 13:12:23 +0300

    Add empty admin panel template. Basic CLI function - specify port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import jsonify
+import argparse
 
 app = Flask(__name__)
 
@@ -19,5 +20,15 @@ def root():
         }])
 
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    parser = argparse.ArgumentParser(description="Run Flask server")
+    parser.add_argument('-p', '--port', default=5000, help='Specify port')
+    
+    args = parser.parse_args()
+
+    app.run(port=args.port)
