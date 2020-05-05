bdfeb8de67c3c608a2072f8a0dfc9c0b3cc44410
psavchuk <savchukdev@gmail.com>
2020-05-05 21:10:02 +0300

    Implement CRUD for news api using binary file

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 from flask import jsonify
 import argparse
+import marshal
+from pathlib import Path
 
 app = Flask(__name__)
 
+NEWS_DUMP_FILE = 'news.dump'
 
 @app.route('/')
-def root():
-    return jsonify([
-        {
-            "id": "1",
-            "title": "First post",
-            "text": "Some text",
-        },
-        {
-            "id": "2",
-            "title": "Second post",
-            "text": "Some text",
-        }])
-
-
-@app.route('/admin')
 def admin():
-    return render_template('admin.html')
+    return render_template('admin.html', 
+                           save_url=url_for('news_save'),
+                           news_url=url_for('news'))
+
+
+@app.route('/api/news')
+def news():
+    dump_file = Path(NEWS_DUMP_FILE)
+    if dump_file.is_file():
+        with open(NEWS_DUMP_FILE, 'rb') as f:
+            result = marshal.load(f)
+    else:
+        result = []
+
+    return jsonify(result)
+
+
+@app.route('/api/news/save', methods=['POST'])
+def news_save():
+    result = []
+    obj = {}
+    
+    for k, v in request.form.items():
+        if k.startswith('node_id'):
+            if len(obj) > 0:
+                result.append(obj)
+
+            obj = { 'id': v }
+        else:
+            braket_index = k.index('[')
+            key = k[:braket_index]
+            obj[key] = v
+
+    if len(obj) > 0:
+        result.append(obj)
+
+    with open(NEWS_DUMP_FILE, 'wb') as f:
+        marshal.dump(result, f)
+
+    return redirect(url_for('admin'))
 
 
 if __name__ == '__main__':
