1e16d4ac15d3f495f8fc60a8bf9320eacdef3f22
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2020-06-11 15:33:17 +0300

    Write an endpoint for publishing news using the app.

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, url_for, redirect, request
 from flask import jsonify
 import argparse
 import marshal
+import json
 from pathlib import Path
 
 app = Flask(__name__)
 
 NEWS_DUMP_FILE = 'news.dump'
 
+
 @app.route('/')
 def admin():
-    return render_template('admin.html', 
+    return render_template('admin.html',
                            save_url=url_for('news_save'),
                            news_url=url_for('news'))
 
@@ -19,8 +21,7 @@ def admin():
 def news():
     dump_file = Path(NEWS_DUMP_FILE)
     if dump_file.is_file():
-        with open(NEWS_DUMP_FILE, 'rb') as f:
-            result = marshal.load(f)
+        result = reader()
     else:
         result = []
 
@@ -31,31 +32,61 @@ def news():
 def news_save():
     result = []
     obj = {}
-    
+
     for k, v in request.form.items():
         if k.startswith('node_id'):
             if len(obj) > 0:
                 result.append(obj)
 
-            obj = { 'id': v }
+            obj = {'id': v}
         else:
-            braket_index = k.index('[')
-            key = k[:braket_index]
+            bracket_index = k.index('[')
+            key = k[:bracket_index]
             obj[key] = v
 
     if len(obj) > 0:
         result.append(obj)
 
-    with open(NEWS_DUMP_FILE, 'wb') as f:
-        marshal.dump(result, f)
+    writer(result)
 
     return redirect(url_for('admin'))
+
+
+@app.route('/api/news/save_by_app', methods=['POST'])
+def news_save_by_app():
+    results = reader()
+
+    if not results:
+        last_id = 1
+    else:
+        last_id = int(results[-1]['id']) + 1
+
+    json_data = json.loads(request.get_data())
+    new_record = {'id': last_id, 'title': list(json_data.keys())[0], 'text': list(json_data.values())[0]}
+
+    results.append(new_record)
+
+    writer(results)
+
+    return {'status_code': 201}
+
+
+def reader():
+    with open(NEWS_DUMP_FILE, 'rb') as f:
+        results = marshal.load(f)
+
+    return results
+
+
+def writer(results):
+    with open(NEWS_DUMP_FILE, 'wb') as f:
+        marshal.dump(results, f)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run Flask server")
     parser.add_argument('-p', '--port', default=5000, help='Specify port')
-    
+
     args = parser.parse_args()
 
     app.run(port=args.port)
